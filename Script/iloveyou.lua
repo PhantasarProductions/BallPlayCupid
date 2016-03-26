2ab7eccee38a3f6fe2171588eb0d5cc0649f0fde
Jeroen Broks <facebook@tbbs.nl>
2016-03-26 17:11:30 +0100

    Init yeah

--- Script/iloveyou.lua
+++ Script/iloveyou.lua
@@ -1,1 +1,9 @@
 -- *import phantasar
+
+
+function love.load()
+assets = phantasar.init({
+                           images = {},
+                           audio  = {}
+                        },"Title")
+end               
